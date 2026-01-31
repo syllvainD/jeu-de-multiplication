860fd889c4a99b4c65e674325e37f34e651ca60d
syllvainD <153740333+syllvainD@users.noreply.github.com>
2026-01-31 15:16:00 +0100

    Update cache name and file list in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,22 @@
-const CACHE_NAME = "multiplication-app-v1";
-const FILES = [
+const CACHE_NAME = "multiplication-game-v1";
+
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
+  "./background.png",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
